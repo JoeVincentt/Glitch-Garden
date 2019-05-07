33231c26d47ab41c42a4d1851fdf7fd6c6bb5342
JoeVincentt <eugene.bb@hotmail.com>
2019-05-07 11:50:08 -0400

    fixed bug with Exceptions

--- Assets/Scripts/Shooter.cs
+++ Assets/Scripts/Shooter.cs
@@ -1,13 +1,65 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Shooter : MonoBehaviour
 {
     [SerializeField] GameObject projectile, gun;
+    AttackerSpawner myLaneSpawner;
 
+    private void Start()
+    {
+        SetLaneSpawner();
+    }
+
+
+    private void Update()
+    {
+        if (IsAttackerInLane())
+        {
+
+        }
+        else
+        {
+
+        }
+    }
+
+    private void SetLaneSpawner()
+    {
+        AttackerSpawner[] spawners = FindObjectsOfType<AttackerSpawner>();
+
+        foreach (AttackerSpawner spawner in spawners)
+        {
+            bool isCloseEnough = (Mathf.Abs(spawner.transform.position.y - transform.position.y) <= Mathf.Epsilon);
+            if (isCloseEnough)
+            {
+                myLaneSpawner = spawner;
+            }
+
+        }
+
+
+
+    }
+
+    private bool IsAttackerInLane()
+    {
+
+        if (myLaneSpawner.transform.childCount <= 0)
+        {
+            return false;
+        }
+        else
+        {
+            return true;
+        }
+
+    }
     public void Fire()
     {
         Instantiate(projectile, gun.transform.position, transform.rotation);
     }
+
 }
